8ff36cf5969190decfdc748214684c435e44392b
Antony <antony.karachansky@spsoft.com>
2022-08-06 11:48:39 +0300

    added demo for permissions

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+import Permissions from './permissions';
+
 function App() {
-  return <div className="App">Nothing to see here</div>;
+  return (
+    <div className="App" style={{ display: 'flex', flexDirection: 'column' }}>
+      <Permissions />
+    </div>
+  );
 }
 
 const container = document.getElementById('root');
---
+++ demo/permissions/index.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+
+import { PermissionCheck, PermissionsProvider } from '../../src';
+
+const initialPermissions = [
+  {
+    action: 'has_this_permission' as any,
+    allowed: true,
+  },
+  {
+    action: 'not_available_permission' as any,
+    allowed: false,
+  },
+  {
+    action: 'no_allowed_specified_permission' as any,
+  },
+];
+const emptyCheckPermissions = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  return [
+    {
+      action: 'checked_on_go' as any,
+      allowed: true,
+    },
+    {
+      action: 'denied_on_go' as any,
+      allowed: false,
+    },
+    {
+      action: 'no_allowed_info_on_go' as any,
+    },
+  ];
+};
+
+const SuccessfullResult = () => {
+  return <span style={{ color: 'lightgreen' }}>✔</span>;
+};
+
+const FailedResult = () => {
+  return <span style={{ color: 'red' }}>❌</span>;
+};
+
+const Permissions = () => {
+  return (
+    <PermissionsProvider
+      initialPermissions={initialPermissions}
+      onCheckPermissions={emptyCheckPermissions}
+    >
+      <h1>Permissions Playground</h1>
+      <PermissionCheck fallback={<FailedResult />} action="has_this_permission">
+        <span>
+          This is permitted <SuccessfullResult />
+        </span>
+      </PermissionCheck>
+      <PermissionCheck action="not_available_permission">
+        This is not permitted, but we see nothing <FailedResult />
+      </PermissionCheck>
+      <PermissionCheck
+        fallback={
+          <span>
+            No Permission <SuccessfullResult />
+          </span>
+        }
+        action="not_available_permission"
+      >
+        <span>
+          This is not permitted, but we have fallback <FailedResult />
+        </span>
+      </PermissionCheck>
+      <PermissionCheck
+        action="didnt check this action"
+        loading={<span>This is loading for 2s</span>}
+        fallback={
+          <span>
+            Server forgot about this permission <SuccessfullResult />
+          </span>
+        }
+      >
+        We didn't get permission from check <FailedResult />
+      </PermissionCheck>
+      <PermissionCheck
+        action="denied_on_go"
+        loading={<span>This is loading for 2s</span>}
+        fallback={
+          <span>
+            Server denied this permission <SuccessfullResult />
+          </span>
+        }
+      >
+        We got refected permission from check <FailedResult />
+      </PermissionCheck>
+      <PermissionCheck
+        action="no_allowed_info_on_go"
+        loading={<span>This is loading for 2s</span>}
+        fallback={
+          <span>
+            Server didn't send allowed check <SuccessfullResult />
+          </span>
+        }
+      >
+        We didn't get allowed permission from check <FailedResult />
+      </PermissionCheck>
+      <PermissionCheck
+        loading={<span>This is loading for 2s</span>}
+        action="checked_on_go"
+        fallback={
+          <span>
+            Permission check failed <FailedResult />
+          </span>
+        }
+      >
+        <span>
+          This content loaded <SuccessfullResult />
+        </span>
+      </PermissionCheck>
+      <PermissionCheck
+        action="checked_on_go"
+        fallback={
+          <span>
+            This message should not appear <FailedResult />
+          </span>
+        }
+      >
+        <span>
+          No Loading content must be shown <SuccessfullResult />
+        </span>
+      </PermissionCheck>
+    </PermissionsProvider>
+  );
+};
+
+export default React.memo(Permissions);
