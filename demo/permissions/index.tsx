a8a351632739ff63729394a963d3637c61347bda
Antony <antony.karachansky@spsoft.com>
2022-10-01 23:21:38 +0300

    added playground for static props

--- demo/permissions/index.tsx
+++ demo/permissions/index.tsx
@@ -43,129 +43,154 @@ const FailedResult = () => {
 
 const Permissions = () => {
   return (
-    <PermissionsProvider
-      initialPermissions={initialPermissions}
-      onCheckPermissions={emptyCheckPermissions}
-    >
-      <h1>Permissions Playground</h1>
-      {/* =============================================================================== */}
-      <PermissionCheck fallback={<FailedResult />} action="has_this_permission">
-        <span>
-          This is permitted <SuccessfullResult />
-        </span>
-      </PermissionCheck>
-      <PermissionCheck action="not_available_permission">
-        This is not permitted, but we see nothing <FailedResult />
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        fallback={
-          <span>
-            No Permission <SuccessfullResult />
-          </span>
-        }
-        action="not_available_permission"
+    <>
+      <PermissionsProvider
+        initialPermissions={initialPermissions}
+        onCheckPermissions={emptyCheckPermissions}
       >
-        <span>
-          This is not permitted, but we have fallback <FailedResult />
-        </span>
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        action="didnt check this action"
-        loading={<span>This is loading for 2s</span>}
-        fallback={
+        <h1>Permissions Playground</h1>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          fallback={<FailedResult />}
+          action="has_this_permission"
+        >
           <span>
-            Server forgot about this permission <SuccessfullResult />
+            This is permitted <SuccessfullResult />
           </span>
-        }
-      >
-        We didn't get permission from check <FailedResult />
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        action="denied_on_go"
-        loading={<span>This is loading for 2s</span>}
-        fallback={
+        </PermissionCheck>
+        <PermissionCheck action="not_available_permission">
+          This is not permitted, but we see nothing <FailedResult />
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          fallback={
+            <span>
+              No Permission <SuccessfullResult />
+            </span>
+          }
+          action="not_available_permission"
+        >
           <span>
-            Server denied this permission <SuccessfullResult />
+            This is not permitted, but we have fallback <FailedResult />
           </span>
-        }
-      >
-        We got refected permission from check <FailedResult />
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        action="no_allowed_info_on_go"
-        loading={<span>This is loading for 2s</span>}
-        fallback={
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action="didnt check this action"
+          loading={<span>This is loading for 2s</span>}
+          fallback={
+            <span>
+              Server forgot about this permission <SuccessfullResult />
+            </span>
+          }
+        >
+          We didn't get permission from check <FailedResult />
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action="denied_on_go"
+          loading={<span>This is loading for 2s</span>}
+          fallback={
+            <span>
+              Server denied this permission <SuccessfullResult />
+            </span>
+          }
+        >
+          We got refected permission from check <FailedResult />
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action="no_allowed_info_on_go"
+          loading={<span>This is loading for 2s</span>}
+          fallback={
+            <span>
+              Server didn't send allowed check <SuccessfullResult />
+            </span>
+          }
+        >
+          We didn't get allowed permission from check <FailedResult />
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          loading={<span>This is loading for 2s</span>}
+          action="checked_on_go"
+          fallback={
+            <span>
+              Permission check failed <FailedResult />
+            </span>
+          }
+        >
           <span>
-            Server didn't send allowed check <SuccessfullResult />
+            This content loaded <SuccessfullResult />
           </span>
-        }
-      >
-        We didn't get allowed permission from check <FailedResult />
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        loading={<span>This is loading for 2s</span>}
-        action="checked_on_go"
-        fallback={
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action={['checked_on_go']}
+          loading={<span>This is loading for 2s</span>}
+          fallback={
+            <span>
+              Permission check failed <FailedResult />
+            </span>
+          }
+        >
           <span>
-            Permission check failed <FailedResult />
+            Action was passed as array <SuccessfullResult />
           </span>
-        }
-      >
-        <span>
-          This content loaded <SuccessfullResult />
-        </span>
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        action={['checked_on_go']}
-        loading={<span>This is loading for 2s</span>}
-        fallback={
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action={['checked_on_go', 'not_available_permission']}
+          loading={<span>This is loading for 2s</span>}
+          isAllowed={allowed =>
+            allowed.includes('checked_on_go') ||
+            allowed.includes('not_available_permission')
+          }
+          fallback={
+            <span>
+              This message should not appear <FailedResult />
+            </span>
+          }
+        >
           <span>
-            Permission check failed <FailedResult />
+            Must handle custom logic <SuccessfullResult />
           </span>
-        }
-      >
-        <span>
-          Action was passed as array <SuccessfullResult />
-        </span>
-      </PermissionCheck>
-      {/* =============================================================================== */}
-      <PermissionCheck
-        action={['checked_on_go', 'not_available_permission']}
-        loading={<span>This is loading for 2s</span>}
-        isAllowed={allowed =>
-          allowed.includes('checked_on_go') ||
-          allowed.includes('not_available_permission')
-        }
-        fallback={
+        </PermissionCheck>
+        {/* =============================================================================== */}
+        <PermissionCheck
+          action="checked_on_go"
+          fallback={
+            <span>
+              This message should not appear <FailedResult />
+            </span>
+          }
+        >
           <span>
-            This message should not appear <FailedResult />
+            No Loading content must be shown <SuccessfullResult />
           </span>
-        }
-      >
-        <span>
-          Must handle custom logic <SuccessfullResult />
-        </span>
-      </PermissionCheck>
+        </PermissionCheck>
+      </PermissionsProvider>
       {/* =============================================================================== */}
-      <PermissionCheck
-        action="checked_on_go"
-        fallback={
+      <PermissionsProvider initialPermissions={initialPermissions}>
+        <PermissionCheck
+          action="this_permission_is_unknown"
+          loading={
+            <span>
+              This message should not appear <FailedResult />
+            </span>
+          }
+          fallback={
+            <span>
+              This message should appear <SuccessfullResult />
+            </span>
+          }
+        >
           <span>
-            This message should not appear <FailedResult />
+            No dynamic check means static permissions <SuccessfullResult />
           </span>
-        }
-      >
-        <span>
-          No Loading content must be shown <SuccessfullResult />
-        </span>
-      </PermissionCheck>
-    </PermissionsProvider>
+        </PermissionCheck>
+      </PermissionsProvider>
+    </>
   );
 };
 
